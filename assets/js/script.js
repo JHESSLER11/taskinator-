2d90f545422529fb92b9ff0cf8080b1c703a6443
JHessler <jhessler11@gmail.com>
2022-01-19 20:16:52 -0500

    updated taskhandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,28 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
 var createTaskHandler = function(event) { 
-  console.log(event);
 
   event.preventDefault();
+  //adds the task name
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  //adds task type to the task name
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  //create item list
   var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
+  listItemEl.className = "task-item";
+
+  //creat dive to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  //git it as class name 
+  taskInfoEl.className = "task-info";
+  //add html content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h3><span class='task-type'>" + taskTypeInput + "</span>";
+  
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
   }; 
 
 formEl.addEventListener("submit", createTaskHandler);
